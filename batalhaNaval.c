3c116e8ff9ada43851b23e642f3e1316ad8a433d
Raissa Sousa <rai7nascimento@gmail.com>
2026-02-04 19:46:58 +0000

    navio diagonal e loops

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,7 +10,7 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    char letras[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    /*char letras[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     //Criando o tabuleiro (Matriz 10x10)
     int tabuleiro[10][10];
@@ -71,12 +71,100 @@ int main() {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
-    }
+    }*/
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+   
+    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    int tabuleiro[10][10];
+    int tamanhoNavio = 3;
+
+    // Inicializa o tabuleiro com 0 (água)
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    // ---------------- NAVIO HORIZONTAL ----------------
+    int linhaH = 2, colunaH = 1;
+
+    if (colunaH + tamanhoNavio > 10) {
+        printf("Erro: navio horizontal fora do tabuleiro.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanhoNavio; i++) {
+        if (tabuleiro[linhaH][colunaH + i] != 0) {
+            printf("Erro: sobreposição de navios.\n");
+            return 1;
+        }
+        tabuleiro[linhaH][colunaH + i] = 3;
+    }
+
+    // ---------------- NAVIO VERTICAL ----------------
+    int linhaV = 5, colunaV = 6;
+
+    if (linhaV + tamanhoNavio > 10) {
+        printf("Erro: navio vertical fora do tabuleiro.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanhoNavio; i++) {
+        if (tabuleiro[linhaV + i][colunaV] != 0) {
+            printf("Erro: sobreposição de navios.\n");
+            return 1;
+        }
+        tabuleiro[linhaV + i][colunaV] = 3;
+    }
+
+    // ---------------- NAVIO DIAGONAL ↘ ----------------
+    int linhaD1 = 4, colunaD1 = 1;
+
+    if (linhaD1 + tamanhoNavio > 10 || colunaD1 + tamanhoNavio > 10) {
+        printf("Erro: navio diagonal fora do tabuleiro.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanhoNavio; i++) {
+        if (tabuleiro[linhaD1 + i][colunaD1 + i] != 0) {
+            printf("Erro: sobreposição de navios.\n");
+            return 1;
+        }
+        tabuleiro[linhaD1 + i][colunaD1 + i] = 3;
+    }
+
+    // ---------------- NAVIO DIAGONAL ↙ ----------------
+    int linhaD2 = 0, colunaD2 = 9;
+
+    if (linhaD2 + tamanhoNavio > 10 || colunaD2 - (tamanhoNavio - 1) < 0) {
+        printf("Erro: navio diagonal fora do tabuleiro.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanhoNavio; i++) {
+        if (tabuleiro[linhaD2 + i][colunaD2 - i] != 0) {
+            printf("Erro: sobreposição de navios.\n");
+            return 1;
+        }
+        tabuleiro[linhaD2 + i][colunaD2 - i] = 3;
+    }
+
+    // ---------------- EXIBIÇÃO DO TABULEIRO ----------------
+    printf("\nTABULEIRO BATALHA NAVAL\n\n");
+    printf("  ");
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", letras[j]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i);
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
